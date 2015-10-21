a32579a90b364447261945545fec0390bda31070
joystrong <shiqiang0711@yahoo.com>
2015-10-21 16:27:02 +0800

    修改

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'qJKqZLgY7iegsfRBI0gyKM09PJaC-bcs',
+            'cookieValidationKey' => 'ivkM_Tj-Wlap5GzhXymUk9xJ9sOUQdYZ',
         ],
     ],
 ];
--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '4RYAmbrRpsvkeBWCnNXXVF6fHK2UH-5c',
+            'cookieValidationKey' => 'SWlcZZAGOHUBZSz8fNnGlEdP70wND-IY',
         ],
     ],
 ];
